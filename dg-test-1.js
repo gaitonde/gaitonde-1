06fc8952ea6e3886032263a665a5b32d699165a1
gaitonde <dayal.gaitonde@gmail.com>
2014-11-04 17:30:36 -0800

    added bower

---
+++ dg-test-1.js
@@ -0,0 +1,8 @@
+(function (window, angular, undefined) {
+  'use strict';
+  console.log('in here1');
+  angular.module('tc.aaf1', ['tc.aaf.auth']);
+  console.log('in here2');
+
+
+})(window, window.angular);
